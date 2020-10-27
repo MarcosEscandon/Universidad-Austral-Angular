84e691503904f7fe4bcadc5b65fc6c336af011ee
Marcos Escandon <marcos.escandon@gmail.com>
2020-10-27 08:33:00 -0300

    fin del Modulo 1

---
+++ angular-wishlist/src/app/destino-viaje/destino-viaje.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { DestinoViaje } from './../models/destino-viaje.model';
+
+@Component({
+  selector: 'app-destino-viaje',
+  templateUrl: './destino-viaje.component.html',
+  styleUrls: ['./destino-viaje.component.css']
+})
+export class DestinoViajeComponent implements OnInit {
+  @Input() destino: DestinoViaje;
+  @HostBinding('attr.class') cssClass = 'col-md-4'
+
+  constructor() {}
+
+  ngOnInit(): {
+  }
+
+}
---
+++ angular-wishlist/src/app/lista-destinos/lista-destinos.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { DestinoViaje } from './../models/destino-viaje.model';
+
+@Component({
+  selector: 'app-lista-destinos',
+  templateUrl: './lista-destinos.component.html',
+  styleUrls: ['./lista-destinos.component.css']
+})
+export class ListaDestinosComponent implements OnInit {
+  destinos: string[];
+  constructor() {
+  	this.destinos = [];
+  	 }
+
+  ngOnInit(): {
+  }
+
+guardar(nombre:string, url:string):boolean {
+	this.destinos.push(new DestinoViaje(nombre, url))
+	return false;
+}
+
+}

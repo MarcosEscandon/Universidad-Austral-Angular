fa0844f75770d052e7dfbf087b98766af4b6487b
Marcos Escandon <marcos.escandon@gmail.com>
2020-11-03 15:55:50 -0300

    Uso de Redux 2da parte

--- angular-wishlist/src/app/lista-destinos/lista-destinos.component.ts
+++ angular-wishlist/src/app/lista-destinos/lista-destinos.component.ts
@@ -1,6 +1,9 @@
 import { Component, EventEmitter, Output, OnInit} from '@angular/core';
 import { DestinoViaje } from './../models/destino-viaje.model';
 import { DestinosApiClient } from "./../models/destinos-api-client.model";
+import { Store } from "@ngrx/store";
+import { AppState } from '../app.module';
+import { ElegidoFavoritoAction, NuevoDestinoAction } from '../models/destinos-viajes-state.model';
 
 @Component({
   selector: 'app-lista-destinos',
@@ -12,15 +15,16 @@ export class ListaDestinosComponent implements OnInit {
   @Output() onItemAdded: EventEmitter<DestinoViaje>;
   updates: string[];  
 
-  constructor(private destinosApiClient:DestinosApiClient) {
+  constructor(private destinosApiClient: DestinosApiClient, private store: Store<AppState>) {
     this.onItemAdded = new EventEmitter();
     this.updates = [];
-    this.destinosApiClient.subscribeOnChange((d: DestinoViaje) => {
-      if (d != null) {
-        this.updates.push('Se ha elegido a ' + d.nombre);
-      }
-    });
-  	 }
+    this.store.select(state => state.destinos.favorito)
+      .subscribe(d => {        
+          if (d != null) {
+            this.updates.push('Se ha elegido a ' + d.nombre);
+          }
+      });
+    }
 
    ngOnInit() {   
    }
@@ -28,10 +32,12 @@ export class ListaDestinosComponent implements OnInit {
   agregado(d: DestinoViaje) {
     this.destinosApiClient.add(d);
     this.onItemAdded.emit(d);
+    this.store.dispatch(new NuevoDestinoAction(d)); 
 }
 
-elegido(e:DestinoViaje) {
+elegido(e: DestinoViaje) {
   this.destinosApiClient.elegir(e);
+  this.store.dispatch(new ElegidoFavoritoAction(e));
 }  
 
 }

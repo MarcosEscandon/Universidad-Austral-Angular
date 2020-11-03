090b22e3255d3f8bd6cc0961516887f2bce02e67
Marcos Escandon <marcos.escandon@gmail.com>
2020-11-03 15:42:24 -0300

    Uso de Redux 1era parte

--- angular-wishlist/src/app/app.module.ts
+++ angular-wishlist/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreModule as NgRxStoreModule, ActionReducerMap } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +11,8 @@ import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
 import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
 import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.component';
 import { FormDestinoViajeComponent } from './form-destino-viaje/form-destino-viaje.component';
-
+import { DestinosApiClient } from './models/destinos-api-client.model';
+import { DestinosViajesEffects, DestinosViajesState, initializeDestinosViajesState, reducerDestinosViajes } from './models/destinos-viajes-state.model';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -17,22 +20,42 @@ const routes: Routes = [
   {path: 'destino', component: DestinoDetalleComponent}, 
 ];
 
+// Redux init
+export interface AppState {
+  destinos: DestinosViajesState;
+}
+
+const reducers: ActionReducerMap<AppState> = {
+  destinos: reducerDestinosViajes
+};
+
+let reducersInitialState = {
+  destinos: initializeDestinosViajesState() 
+};
+
+// Redux fin init
+
 @NgModule({
   declarations: [
     AppComponent,
     DestinoViajeComponent,
     ListaDestinosComponent,
     DestinoDetalleComponent,
-    FormDestinoViajeComponent
+    FormDestinoViajeComponent,
+    NgRxStoreModule
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(routes) 
+    RouterModule.forRoot(routes),
+    NgRxStoreModule.forRoot(reducers, {initialState: reducersInitialState}),
+    EffectsModule.forRoot([DestinosViajesEffects]) 
   ],
-  providers: [],
+  providers: [
+    DestinosApiClient
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

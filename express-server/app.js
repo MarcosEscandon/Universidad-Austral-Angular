fe045d83929ec78792e0c5f0a5f892c34e3e0913
Marcos Escandon <marcos.escandon@gmail.com>
2020-11-28 08:47:30 -0300

    curso 2 mod4 lecc4 vid07 grabacion circleci

---
+++ express-server/app.js
@@ -0,0 +1,21 @@
+var express = require("express"), cors = require('cors');
+var app = express();
+app.use(express.json());
+app.use(cors());
+app.listen(3000, () => console.log("Server running on port 3000"));
+
+var ciudades = ["Paris", "Barcelona", "Madrid", "Roma", "Amsterdam", "Nueva York", "San Andres"]
+app.get("/ciudades", (req, res, next) => res.json(ciudades.filter((c) => c.toLowerCase().indexOf(
+    req.query.q.toString().toLowerCase()) > -1)));
+
+var misDestinos = [];
+app.get("/my", (req, res, next) => res.json(misDestinos));
+app.post("/my", (req, res, next) => {
+    console.log(req.body);
+    misDestinos.push(req.body.nuevo);
+    res.json(misDestinos);
+});
+
+app.get("/api/translation", (req, res, next) => res.json([
+    {lang: req.query.lang, key: 'HOLA', value: 'HOLA' + req.query.lang}
+]));
--- readme Angular.txt
+++ readme Angular.txt
@@ -1,3 +1,5 @@
+cd ../Documents\Web-develop\Angular\curso-angular6
+
 -g --> a nivel global en toda la maquina
 
 cd ~ --> directorio de usuario.
@@ -11,4 +13,6 @@ git pull --> Trae los cambios desde el servidor a la pc local.
 ng --version --> version de Angular
 ng --help --> lista de comandos disponibles.
 ng "comando" --help --> ayuda sobre ese comando.
-ng new "directorio" --> Nueva Carpeta virgen de Angular con convencion sobre configuracion
+ng new "directorio" --> Nueva Carpeta virgen de Angular con convencion sobre configuracion.
+ng serve --> empaqueta todo el codigo y lo sirve en un server dev.
+ng generate component "nombre" --> genera, un componente llamado. Se puede poner: g c
